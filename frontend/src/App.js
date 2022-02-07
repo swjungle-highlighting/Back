c446a8ac81478168a15bdae2b7b276db9dd2c123
DongGeun974 <zxcasd3004@naver.com>
2022-02-07 21:58:21 +0900

    'testing'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from "react";
 import axios from 'axios'
@@ -6,30 +5,57 @@ import axios from 'axios'
 function App() {
 
   const [getMessage, setGetMessage] = useState({})
+  const [url, setUrl] = useState('URL : ')
 
-  useEffect(()=>
-  {
-    axios.get('http://localhost:5000/flask/hello').then(response =>
+  function onChangeUrl(e){
+    console.log('call onChangeUrl()')
+    if (e.target.value.indexOf('youtube') != -1)
     {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error =>
-    {
-      console.log(error)
-    })
-  }, [])
+      console.log('This is Youtube link')
+    //  app header class 값을 변경하면
+
+    }
+    setUrl('URL : ' + e.target.value)
+  }
+
+  function sendUrl(e){
+    console.log(e)
+    const url = document.getElementById('link')
+
+    console.log('call sendUrl()' + ' ' + this.url)
+    console.log(url.innerText)
+  }
+
+  // useEffect(()=>
+  // {
+  //   axios.get('http://localhost:5000/flask/hello').then(response =>
+  //   {
+  //     console.log("SUCCESS", response)
+  //     setGetMessage(response)
+  //   }).catch(error =>
+  //   {
+  //     console.log(error)
+  //   })
+  // }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
-          React + Flask Tutorial
+          {/*React + Flask Tutorial*/}
+          link input
         </p>
-        <div>{getMessage.status === 200 ?
-        <h3>{getMessage.data.message}</h3>
-        :<h3>LOADING</h3>}
-        </div>
+
+        <input onChange={onChangeUrl} id='link'/>
+        <h3>{url}</h3>
+        <button onClick={sendUrl}>버튼</button>
+
+        {/*<div>{getMessage.status === 200 ?*/}
+        {/*<h3>{getMessage.data.message}</h3>*/}
+        {/*:<h3>LOADING</h3>}*/}
+        {/*</div>*/}
+
       </header>
     </div>
   );
