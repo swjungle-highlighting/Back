007cdc1e4bce651e379e40540bd7e4fb2eb20657
Eithea <oguraff94@gmail.com>
2022-02-27 08:53:29 +0900

    cutTool 다운로드 구현

--- app.py
+++ app.py
@@ -7,12 +7,14 @@
 @Date ：2022-02-07 오후 5:28 
 '''
 
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, send_file
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
 from api.HelloApiHandler import HelloApiHandler
 from api.KeywordsApiHandler import KeywordsApiHandler
 from api.DownloadApiHandler import DownloadApiHandler
+
+from api.download_logic.do_logic import create_cutTool, delete_cutTool
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app)
@@ -25,3 +27,7 @@ def serve(path):
 api.add_resource(HelloApiHandler, '/flask/hello')
 api.add_resource(KeywordsApiHandler, '/flask/keywords')
 api.add_resource(DownloadApiHandler, '/flask/download')
+
+@app.route("/downloadpath")
+def download() :
+    return send_file("HIGHLIGHTING.zip")
